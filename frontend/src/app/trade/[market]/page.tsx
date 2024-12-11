ffd34698396e85fdaed5fa9afc915ffd920a95d3
AaraV <aaravrawat00000@gmail.com>
2024-12-11 10:00:00 -0500

    deleted extra folders-files

--- frontend/src/app/trade/[market]/page.tsx
+++ frontend/src/app/trade/[market]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { MarketBar } from "@/app/components/MarketBar";
-import { SwapUI } from "@/app/components/SwapUI";
-import { TradeView } from "@/app/components/TradeView";
+// import { SwapUI } from "@/app/components/SwapUI";
+// import { TradeView } from "@/app/components/TradeView";
 import { Depth } from "@/app/components/depth/Depth";
 import { useParams } from "next/navigation";
 
@@ -13,7 +13,7 @@ export default function Page() {
             <MarketBar market={market as string} />
             <div className="flex flex-row h-[620px] border-y border-slate-800">
                 <div className="flex flex-col flex-1">
-                    <TradeView market={market as string} />
+                    {/* <TradeView market={market as string} /> */}
                 </div>
                 <div className="w-[1px] flex-col border-slate-800 border-l"></div>
                 <div className="flex flex-col w-[250px] overflow-hidden">
@@ -24,7 +24,7 @@ export default function Page() {
         <div className="w-[1px] flex-col border-slate-800 border-l"></div>
         <div>
             <div className="flex flex-col w-[250px]">
-                <SwapUI market={market as string} />
+                {/* <SwapUI market={market as string} /> */}
             </div>
         </div>
     </div>
